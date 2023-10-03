80f1cc09100efb85dc36b244923774a3c3fb30c4
etorres12345 <erik.torres1215@gmail.com>
2023-10-03 17:51:04 +0200

    delete restaurant function add

--- src/pages/TripDetailsPage.jsx
+++ src/pages/TripDetailsPage.jsx
@@ -31,6 +31,20 @@ function TripDetailsPage(props) {
     getTrip();
   }, []);
 
+  const handleDeleteRestaurant = (placeId) => {
+    axios
+      .delete(`${API_URL}/api/trips/${tripId}/restaurants/$placeId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then(() => {
+        setTrip((prevTrip) => ({
+          ...prevTrip,
+          places: prevTrip.places.filter((place) => place.place_id !== placeId),
+        }));
+      })
+      .catch((error) => console.error("Error deleting restaurant:", error));
+  };
+
   return (
     <div className="TripDetails">
       {trip && (
@@ -44,8 +58,13 @@ function TripDetailsPage(props) {
           <li className="PlaceCard" key={place.place_id}>
             <h2>{place.name}</h2>
             <p>Address: {place.formatted_address}</p>
-            <p>Restaurant Rating: {place.rating}</p>
+            <p>Restaurant Rating: {place.rating}/5 stars!</p>
             <p>Price Level: {place.price_level || "N/A"}</p>
+            <p>
+              {place.opening_hours.open_now === true
+                ? "open now!"
+                : "Not open at this hour, check again later!"}
+            </p>
             {place.photos?.map((photo, photoIndex) => (
               <img
                 key={photoIndex}
@@ -53,6 +72,9 @@ function TripDetailsPage(props) {
                 alt={`Photo ${photoIndex}`}
               />
             ))}
+            <button onClick={() => handleDeleteRestaurant(place.place_id)}>
+              Delete Restaurant
+            </button>
           </li>
         ))}
       <Link to="/trips">
